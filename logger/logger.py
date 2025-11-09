1f253ecf7110091693d62737b659446996694196
Sofia Nelipovich <sofia.nelipovich@gmail.com>
2025-11-09 21:30:45 +0300

    logger fix

--- logger/logger.py
+++ logger/logger.py
@@ -4,7 +4,13 @@ import wandb
 class WandbLogger:
     def __init__(self, project="lora_demo", run_name=None):
         self.wandb = wandb
-        self.run = wandb.init(project=project, name=run_name)
+        self.run = wandb.init(
+            project=project, 
+            name=run_name,
+            monitor_gym=True,
+            save_code=True,
+            settings=wandb.Settings(code_dir=".")
+        )
 
     def log_config(self, params):
         self.wandb.config.update(params)
